32efd0d19e3633e22ed7887eaf3dad9cbc6805b8
boscomol <akbar22rivaldy@gmail.com>
2020-07-30 07:49:15 +0700

    Website PointBlank Version 1.0

---
+++ index.php
@@ -0,0 +1,374 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"
+    />
+    <link rel="stylesheet" href="style.css">
+
+    <title>Point Blank - Indonesia</title>
+  </head>
+  <body>
+    
+    
+
+
+<!-- Navbar -->
+
+  <div class="my-navbar">
+    <nav id="navbar-example" class="navbar navbar-expand-lg menu">
+      <div class="container-fluid">
+        <a class="navbar-brand text-uppercase" href="#"><img src="assets/logo_pointblank.png"> </a><span class="badge bg-success">Indonesia</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto animate__animated animate__fadeInDown">
+            <a class="nav-link" href="index.php">Home</a>
+            <a class="nav-link" href="#">Download</a>
+            <a class="nav-link" href="#">Login</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+<!-- Akhir Navbar -->
+
+
+
+<!-- Toogle Cookies
+
+<div class="cookie">
+  <div class="alert alert-info alert-dismissible fade show" role="alert">
+    <strong>This Website is use Cookies!</strong> For more best experience of this website, we use cookies.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">Ok, boomer!</span>
+    </button>
+  </div>
+</div>
+
+-->
+
+
+
+
+<!-- Jumbotron -->
+
+  <div class="jumbotron text-center">
+    <h1 class="display-3 text-white user-select-none wow animate__animated animate__jackInTheBox">Point Blank <span>Beyond Limits</span></h1>
+    <p class="lead text-white user-select-none wow animate__animated animate__slideInUp">Zepetto Interactive</p>
+    <a class="btn btn-outline-light wow animate__animated animate__zoomIn" href="#" style="border-radius: 0;">Daftar Akun</a>
+    <a class="btn btn-light wow animate__animated animate__zoomIn" href="#" style="border-radius: 0; margin-left: 10px;">Login</a>
+  </div>
+
+<!-- Akhir Jumbotron -->
+
+
+
+<!-- Konten -->
+
+  <div id="content" class="konten">
+
+    <!-- Status -->
+      <div id="status" class="row text-white text-center">
+        <div class="col-md-4">
+          <div class="card wow animate__animated animate__zoomIn" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title user-select-none font-weight-bold">Server</h5>
+              
+              <p class="card-text user-select-none text-primary">Singapore</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card wow animate__animated animate__zoomIn" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title user-select-none font-weight-bold">Status Server</h5>
+              
+              <p class="card-text user-select-none text-success">Online</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card wow animate__animated animate__zoomIn" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title user-select-none font-weight-bold">Player Online</h5>
+              
+              <p class="card-text user-select-none text-info">339</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Akhir Status -->
+
+      <!-- About -->
+      <div id="about" class="row">
+        <div class="col-12 wow animate__animated animate__lightSpeedInLeft">
+          <h2 class="text-white font-weight-bold">Tentang Kami</h2>
+          <p class="lead text-white"><a href="index.php">Evolved Point Blank</a> merupakan game First Person Shooter (FPS) berbasis Private Server di Indonesia. Evolved PB menjadi PB Private Server yang menarik dan akan berusaha lebih baik dengan PB Private Server lainnya. Tunggu apalagi? Bergabung dan bermain bersama Pemain lainnya di Evolved PB.</p>
+        </div>
+      </div>
+      <!-- Akhir About -->
+
+
+      <!-- Tabel Rank -->
+      <div id="rank" class="row">
+        <div class="col-12">
+          <h2 class="text-white font-weight-bold wow animate__animated animate__slideInLeft">Top 10 Rank Individu</h2>
+          <hr>
+          <div class="container">
+            <table class="table table-borderless table-hover text-white mt-4 text-center" border="0">
+            <thead>
+              <tr>
+                <th scope="col">Ranking</th>
+                <th scope="col">Nickname</th>
+                <th scope="col">EXP</th>
+                <th scope="col">Rank</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Dani Geode</td>
+                <td>373,410,648</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/52.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>P s y c h ш ш™</td>
+                <td>373,101,559</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/52.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>NB ~є A B A H є~</td>
+                <td>361,173,790</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/52.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>`iBaaaL_</td>
+                <td>273,149,927</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/51.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">5</th>
+                <td>R  O  X  A  N  E</td>
+                <td>257,655,698</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/51.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">6</th>
+                <td>+NATO+NR_~</td>
+                <td>246,330,101</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/51.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">7</th>
+                <td>CARAMELBISCUIT</td>
+                <td>232,925,300</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/51.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">8</th>
+                <td>Magic`TRFs_</td>
+                <td>228,659,820</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/51.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">9</th>
+                <td>Secret OdэnGOD+</td>
+                <td>227,611,046</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/51.gif"></td>
+              </tr>
+              <tr>
+                <th scope="row">10</th>
+                <td>||P I L G U D||</td>
+                <td>220,662,712</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/player/51.gif"></td>
+              </tr>
+            </tbody>
+          </table>
+          </div>
+
+          <h2 class="text-white font-weight-bold wow animate__animated animate__slideInLeft" style="margin-top: 100px;">Top 10 Rank Clan</h2>
+          <hr>
+          <div class="container">
+            <table class="table table-borderless table-hover text-white mt-4 text-center" border="0">
+            <thead>
+              <tr>
+                <th scope="col">Ranking</th>
+                <th scope="col">Clan Name</th>
+                <th scope="col">EXP</th>
+                <th scope="col">Rank</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>[M]-RedNine</td>
+                <td>4,721,837</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/19.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>'AMEZINNG333</td>
+                <td>4,643,904</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/19.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>NightBastrad146™</td>
+                <td>4,630,150</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/19.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>G e o d e ™</td>
+                <td>4,582,858</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/19.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">5</th>
+                <td>MARA+SALVATRUCHA</td>
+                <td>3,307,189</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/16.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">6</th>
+                <td>++GLADIATORS++</td>
+                <td>2,863,633</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/15.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">7</th>
+                <td>_SOUTHBORNEO</td>
+                <td>2,700,273</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/15.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">8</th>
+                <td>WEDONTCARE`766</td>
+                <td>2,562,386</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/15.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">9</th>
+                <td>-iMERCILESSLY291</td>
+                <td>2,458,712</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/14.jpg"></td>
+              </tr>
+              <tr>
+                <th scope="row">10</th>
+                <td>¤єQueen¤§</td>
+                <td>2,422,046</td>
+                <td><img src="http://evolvedpb.com/assets/images/rank/clan/14.jpg"></td>
+              </tr>
+            </tbody>
+          </table>
+          </div>
+        </div>
+      </div>
+      <!-- Akhir Tabel Rank -->
+
+  </div>
+
+  <!-- Komunitas -->
+  <div id="komunitas" class="row">
+    <div class="row text-center">
+      <div class="col-12">
+        <h3 class="wow animate__animated animate__fadeInUp user-select-none">Gabung ke Komunitas Kami</h3>
+        <a href="http://discord.com" target="_blank" class="wow animate__animated animate__fadeInLeft"><i class="fab fa-discord"></i></a>
+        <a href="http://facebook.com" target="_blank" class="wow animate__animated animate__fadeInRight"><i class="fab fa-facebook"></i></a>
+      </div>
+    </div>
+  </div>
+  <!-- Akhir Komunitas -->
+
+<!-- Akhir Konten -->
+
+
+
+
+<!-- Footer -->
+
+  <footer>
+    <div class="copyright">
+      <p>Copyright &copy; <?php echo date('Y'); ?> - All Rights Reserved </p>
+    </div>
+    <div class="company">
+      <p>PT. Zepetto Interactive Indonesia</p>
+    </div>
+  </footer>
+
+<!-- Akhir Footer -->
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- Popper.js first, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+
+
+    <!-- Custom JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+
+    <script>
+      var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       0,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+          // the callback is fired every time an animation is started
+          // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null,    // optional scroll container selector, otherwise use window,
+        resetAnimation: true,     // reset animation on end (default is true)
+      }
+    );
+    wow.init();
+    </script>
+
+
+
+    <script
+  src="https://code.jquery.com/jquery-3.5.1.js"
+  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
+  
+  <script>
+  $(document).ready(function(){
+  $(window).scroll(function(){
+  if($(window).scrollTop() > 60 ){
+  $('.my-navbar').addClass('navbar-scroll');
+  }
+  else{
+  $('.my-navbar').removeClass('navbar-scroll');
+  }
+  });
+  });
+  
+  </script>
+
+  <script src="https://kit.fontawesome.com/3f5bf5ad7b.js" crossorigin="anonymous"></script>
+
+  </body>
+</html>
